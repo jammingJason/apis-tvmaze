6b2fbbe1f7a055c3dc6bd910548447f8388e40b1
Van Jason Crews <vanjasoncrews@gmail.com>
2022-08-19 11:14:58 -0400

    finished files

--- tvmaze.js
+++ tvmaze.js
@@ -2,7 +2,6 @@
  *     { id, name, summary, episodesUrl }
  */
 
-
 /** Search Shows
  *    - given a search term, search for tv shows that
  *      match that query.  The function is async show it
@@ -18,73 +17,91 @@
       }
  */
 async function searchShows(query) {
-  // TODO: Make an ajax request to the searchShows api.  Remove
-  // hard coded data.
-
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+  const res = await axios.get('http://api.tvmaze.com/search/shows?q=' + query);
+  let arrShows = [];
+  for (let i = 0; i < res.data.length; i++) {
+    let newShow = res.data[i].show;
+    let newImage = newShow.image;
+    if (newImage === null) {
+      newImage = 'https://tinyurl.com/tv-missing';
+    } else {
+      newImage = newShow.image.medium;
     }
-  ]
+    arrShows.push({
+      id: newShow.id,
+      name: newShow.name,
+      summary: newShow.summary,
+      image: newImage,
+    });
+  }
+  console.log(arrShows);
+  return arrShows;
 }
-
-
 
 /** Populate shows list:
  *     - given list of shows, add shows to DOM
  */
 
 function populateShows(shows) {
-  const $showsList = $("#shows-list");
+  const $showsList = $('#shows-list');
   $showsList.empty();
-
+  const modalTitle = document.querySelector('#modalLabel');
   for (let show of shows) {
+    const intID = Math.floor(Math.random() * 1111);
+    modalTitle.innerText = show.name;
     let $item = $(
       `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
          <div class="card" data-show-id="${show.id}">
            <div class="card-body">
+           <img src="${show.image}">
              <h5 class="card-title">${show.name}</h5>
              <p class="card-text">${show.summary}</p>
-           </div>
+             <button id="${intID}" class="btn btn-primary" data-toggle="modal"
+             data-target="#exampleModal">Get Episodes</button>
+             </div>
          </div>
        </div>
-      `);
-
+      `
+    );
+    // alert(show.image);
     $showsList.append($item);
+    const episodeBtn = document.getElementById(intID);
+    episodeBtn.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      $('#episodes-area').show();
+      getEpisodes(show.id);
+    });
   }
 }
-
 
 /** Handle search form submission:
  *    - hide episodes area
  *    - get list of matching shows and show in shows list
  */
 
-$("#search-form").on("submit", async function handleSearch (evt) {
+$('#search-form').on('submit', async function handleSearch(evt) {
   evt.preventDefault();
-
-  let query = $("#search-query").val();
+  let query = $('#search-query').val();
   if (!query) return;
-
-  $("#episodes-area").hide();
-
+  $('#episodes-area').hide();
   let shows = await searchShows(query);
-
   populateShows(shows);
 });
-
 
 /** Given a show ID, return list of episodes:
  *      { id, name, season, number }
  */
 
 async function getEpisodes(id) {
+  const getUL = document.querySelector('#episodes-list');
   // TODO: get episodes from tvmaze
   //       you can get this by making GET request to
   //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
-
+  const res = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`);
+  for (let i = 0; i < res.data.length; i++) {
+    const newLI = document.createElement('li');
+    newLI.innerText = res.data[i].name + ' - Season: ' + res.data[i].season;
+    getUL.append(newLI);
+  }
   // TODO: return array-of-episode-info, as described in docstring above
 }
